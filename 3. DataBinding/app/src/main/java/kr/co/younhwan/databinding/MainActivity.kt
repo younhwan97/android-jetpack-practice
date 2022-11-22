550b0b2a38bf4863a388d2d9493633d99f52668d
younhwan97 <younhwan0903@gmail.com>
2022-11-22 18:12:23 +0900

    DataBinding

---
+++ 3. DataBinding/app/src/main/java/kr/co/younhwan/databinding/MainActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.younhwan.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.younhwan.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // DataBinding
+        // ViewBinding과 차이? -> 이름처럼 데이터를 연결해주는 역할을 할 수 있다. (데이터와 같이 결합해서 사용할 수 있음)
+        // build.gradle에 DataBinding 사용 선언!
+        // layout 파일 상위에 layout 태그 추가
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dataBindingEx.text = "바뀐 텍스트"
+    }
+}
