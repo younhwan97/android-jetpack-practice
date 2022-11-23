b66f34b8e6ddccdcae972758ded15390c1dcdb06
younhwan97 <younhwan0903@gmail.com>
2022-11-23 20:58:23 +0900

    DataBinding

--- 3. DataBinding/app/src/main/java/kr/co/younhwan/databinding/MainActivity.kt
+++ 3. DataBinding/app/src/main/java/kr/co/younhwan/databinding/MainActivity.kt
@@ -3,6 +3,8 @@ package kr.co.younhwan.databinding
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import kr.co.younhwan.databinding.databinding.ActivityMainBinding
 
@@ -22,5 +24,15 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+
+        // 아직까지는 ViewBinding과의 차이가 없음
+        // 데이터 클래스를 생성하고 layout의 data 태그 아래 variable 태그에 데이터를 연결해줌으로써
+        // DataBinding의 진가가 발휘됨
+        val user = Person("조윤환", 26)
+        binding.user = user
+    }
+
+    fun myClick(view: View) {
+        Log.d("click", "myclick")
     }
 }
---
+++ 3. DataBinding/app/src/main/java/kr/co/younhwan/databinding/Person.kt
@@ -0,0 +1,6 @@
+package kr.co.younhwan.databinding
+
+data class Person(
+    var name : String,
+    var age : Int
+)
